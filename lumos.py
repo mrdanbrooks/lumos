491afe6bc4f2ddcc1a37772258ce6daccb9a7b8b
dan brooks <dan@danbrooks.net>
2014-12-01 16:09:50 -0500

    configures lumos to log to syslog

--- lumos.py
+++ lumos.py
@@ -2,10 +2,11 @@
 """ lumos - udp interface for bottlerocket x10 control
 
 TODO:
-    - commandline args to br command and allowed expressions
+    - commandline args to br command and allowed expressions, and listening port
 """
 import re
 import subprocess
+import syslog
 from twisted.internet import reactor
 from twisted.internet.protocol import DatagramProtocol
 
@@ -15,21 +16,21 @@ allowed = re.compile('^m[123](,m[123])* on$')
 # bottle rocket command
 br_cmd = "br -x/dev/ttyUSB0"
 
+
 class LumosReceiver(DatagramProtocol):
     def datagramReceived(self, data, (host, port)):
-        # Filter input for allowed commands
         if allowed.search(data) is None:
-            print "Rejected %s" % data
+            syslog.syslog(syslog.LOG_ALERT, "Lumos: Rejected '%s' sent from host %s" % (data, host))
             return
-        print "Received %s" % data
-        ret = subprocess.call(br_cmd+" "+data,
+        cmd = br_cmd+" "+data.strip()
+        ret = subprocess.call(cmd,
                               shell=True,
                               stdout=open('/dev/null', 'w'),
                               stderr=subprocess.STDOUT)
-        if not ret == 0:
-            print "Command Failed!"
+        if ret == 0:
+            syslog.syslog("Lumos: Called '%s' from %s" % (cmd, host))
         else:
-            print "success"
+            syslog.syslog(syslog.LOG_ALERT, "Lumos: Failed to call '%s' from %s" % (cmd, host))
 
 
 reactor.listenMulticast(8005, LumosReceiver(), listenMultiple=True)
